ca6c55c0b3a183493ce5855306a918c1e1ca1b32
IShin00biI <micnar12@gmail.com>
2017-08-20 17:31:33 +0200

    [part2] models division

--- part2/main/admin.py
+++ part2/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import County, Area, Commune
+
 # Register your models here.
+admin.site.register(County)
+admin.site.register(Area)
+admin.site.register(Commune)
--- part2/main/models.py
+++ part2/main/models.py
@@ -1,25 +1,39 @@
 from django.db import models
 
-class Commune(models.Model):
-    county = models.CharField(max_length=32)
-    area = models.IntegerField()
-    name = models.CharField(max_length=255)
-    subareas = models.IntegerField()
-    people = models.IntegerField()
-    cards = models.IntegerField()
-    invalid = models.IntegerField()
-    grabowski = models.IntegerField()
-    ikonowicz = models.IntegerField()
-    kalinowski = models.IntegerField()
-    korwin = models.IntegerField()
-    krzaklewski = models.IntegerField()
-    kwasniewski = models.IntegerField()
-    lepper = models.IntegerField()
-    lopuszanski = models.IntegerField()
-    olechowski = models.IntegerField()
-    pawlowski = models.IntegerField()
-    walesa = models.IntegerField()
-    wilecki = models.IntegerField()
+
+class County(models.Model):
+    name = models.CharField(max_length=32, primary_key=True)
 
     def __str__(self):
-        return "Commune %s" % self.name
+        return "Województwo %s" % self.name
+
+
+class Area(models.Model):
+    county = models.ForeignKey(County, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Okręg %s" % self.id
+
+
+class Commune(models.Model):
+    area = models.ForeignKey(Area, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    subareas = models.IntegerField('Obwody')
+    people = models.IntegerField('Uprawnieni')
+    cards = models.IntegerField('Karty wydane')
+    invalid = models.IntegerField('Głosy nieważne')
+    grabowski = models.IntegerField('Dariusz Maciej GRABOWSKI')
+    ikonowicz = models.IntegerField('Piotr IKONOWICZ')
+    kalinowski = models.IntegerField('Jarosław KALINOWSKI')
+    korwin = models.IntegerField('Janusz KORWIN-MIKKE')
+    krzaklewski = models.IntegerField('Marian KRZAKLEWSKI')
+    kwasniewski = models.IntegerField('Aleksander KWAŚNIEWSKI')
+    lepper = models.IntegerField('Andrzej LEPPER')
+    lopuszanski = models.IntegerField('Jan ŁOPUSZAŃSKI')
+    olechowski = models.IntegerField('Andrzej Marian OLECHOWSKI')
+    pawlowski = models.IntegerField('Bogdan PAWŁOWSKI')
+    walesa = models.IntegerField('Lech Wałęsa')
+    wilecki = models.IntegerField('Tadeusz Adam WILECKI')
+
+    def __str__(self):
+        return "Gmina %s" % self.name
